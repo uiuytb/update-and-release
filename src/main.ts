cc6e122e053007403bc7b2f75a1bbe150bf9d693
uiuytb <uiuytb@gmail.com>
2023-03-19 16:57:31 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {readFileSync} from 'fs'
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+    try {
+        core.debug(readFileSync('package.json', {encoding: 'utf8'}))
+    } catch (error) {
+        if (error instanceof Error) core.setFailed(error.message)
+    }
 }
 
 run()
