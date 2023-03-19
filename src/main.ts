a61078418d5e4e3299cd5bcc966e2bb752c37ea1
uiuytb <uiuytb@gmail.com>
2023-03-19 17:14:25 +0100

    last

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import {readFileSync} from 'fs'
 
 async function run(): Promise<void> {
     try {
-        core.debug(readFileSync('package.json', {encoding: 'utf8'}))
+        const pkg = JSON.parse(readFileSync('package.json', {encoding: 'utf8'}))
+        core.debug(pkg.version)
+        core.info(JSON.stringify(github.context))
     } catch (error) {
         if (error instanceof Error) core.setFailed(error.message)
     }
